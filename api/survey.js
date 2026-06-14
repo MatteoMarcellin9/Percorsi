873530833273275ed076b6d11f24777c4521e8d0
MatteoMarcellin9 <134493395+MatteoMarcellin9@users.noreply.github.com>
2026-06-14 12:55:10 +0200

    Add PATCH endpoint for editing comment category

--- api/survey.js
+++ api/survey.js
@@ -31,7 +31,7 @@ async function saveComments(comments, sha) {
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
 
